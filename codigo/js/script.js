214e9cfc6f61bf3805436318ecd5be8b48a651b8
ErickEDM <l17112382@cdjuarez.tecnm.mx>
2020-10-30 00:03:15 -0600

    Cuento

---
+++ codigo/js/script.js
@@ -0,0 +1,15 @@
+console.log(5);
+console.log("Hola mundo");
+console.log("Honla mnundo".lastIndexOf("n"));
+console.log(255.255);
+console.log(2+3);
+console.log(70-12);
+//Tipos de datos: Numéricos, String, Booleanos, Null, undefined, symbol, object
+//Variables
+var x = 11;
+x = "Hola"
+let y = 10;
+const vacio = false;
+/*
+	comentarios
+*/
